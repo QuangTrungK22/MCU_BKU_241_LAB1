eba64f6a78916de7dd21a6a3431189f1adf6c006
DESKTOP-PC9T07E\win <trung.lamcsek22@hcmut.edu.vn>
2024-09-17 13:32:20 +0700

    Init for LAB1_MCU

---
+++ Lab1/Core/Inc/LED.h
@@ -0,0 +1,16 @@
+/*
+ * LED.h
+ *
+ *  Created on: Sep 9, 2024
+ *      Author: win
+ */
+
+#ifndef INC_LED_H_
+#define INC_LED_H_
+#include "main.h"
+void Exercise1(void);
+void Exercise2(int num);
+void Exercise3(void);
+void display7SEG(int num);
+void Exercise4();
+#endif /* INC_LED_H_ */
